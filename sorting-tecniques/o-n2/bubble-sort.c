81235525d5eed0fd3b5634a2a58497828070c0a6
ralvarezdev <86166683+ralvarezdev@users.noreply.github.com>
2023-10-12 14:23:35 -0400

    feat: Added Bubble Sort and Insertion Sort
    fix: Added Missing Operator in Bubble Sort

---
+++ sorting-tecniques/o-n2/bubble-sort.c
@@ -0,0 +1,26 @@
+// Bubble Sort
+// O(n^2)
+// Stable
+void bubbleSort(int A[], int n)
+{
+
+  int i, j, flag;
+
+  for (i = 0; i < n - 1; i++)
+  {
+    flag = 0;
+
+    for (j = 0; j < n - 1 - i; j++)
+    {
+      if (A[j] > A[j + 1])
+      {
+        swap(&A[j], &A[j + 1]);
+        flag = 1;
+      }
+    }
+    if (flag == 0)
+    {
+      break;
+    }
+  }
+}
---
+++ sorting-tecniques/o-n2/insertion-sort.c
@@ -0,0 +1,20 @@
+// Insertion Sort
+// O(n^2)
+// Adaptive and Stable
+void insertionSort(int A[], int n)
+{
+  int j, x;
+
+  for (int i = 1; i < n; i++)
+  {
+    j = i - 1;
+    x = A[i];
+
+    while (j > -1 && A[j] > x)
+    {
+      A[j + 1] = A[j];
+      j--;
+    }
+    A[j + 1] = x;
+  }
+}
